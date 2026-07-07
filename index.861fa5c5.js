247ead28f3752133b6c613aff872cebfe6c7865f
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-07-07 20:59:56 +0300

    make build

---
+++ index.861fa5c5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientWidth-t.offsetWidth)/2;t.style.left="".concat(c,"px"),t.style.top="".concat(c,"px");
+//# sourceMappingURL=index.861fa5c5.js.map
